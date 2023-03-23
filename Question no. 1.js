3ff0126410e9c1874c86c256b7573ae3326d5d0f
Tkhalid993 <85389439+Tkhalid993@users.noreply.github.com>
2023-03-23 15:13:39 +0500

    Add files via upload

---
+++ Question no. 1.js
@@ -0,0 +1,18 @@
+
+
+
+// To check you're eligible for vote or not
+
+
+
+let age = parseInt(prompt("Enter your age."));
+
+if (age>17){
+  console.log("You're eligible for vote.");
+}
+else{
+  console.log("You're not eligible for vote.")
+}
+
+
+
---
+++ Question no. 3.js
@@ -0,0 +1,10 @@
+// Table 
+
+
+let num = parseInt(prompt("Enter any number."));
+
+for(i=1; i<=10; i++){
+  console.log(num," * ", i, " =", num*i);
+  
+}
+
